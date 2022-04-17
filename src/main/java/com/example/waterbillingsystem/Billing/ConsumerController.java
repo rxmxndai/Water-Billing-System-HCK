96043d2ca9c4aedc6467eab6a5ea741dbdad50d5
SaugatSp <np03cs4s210146@heraldcollege.edu.np>
2022-04-17 15:24:01 +0545

    loginpage done

--- src/main/java/com/example/waterbillingsystem/Billing/ConsumerController.java
+++ src/main/java/com/example/waterbillingsystem/Billing/ConsumerController.java
@@ -3,9 +3,8 @@ package com.example.waterbillingsystem.Billing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -30,6 +29,22 @@ public class ConsumerController {
 
 
     @Autowired private ConsumerService service;
+
+    @GetMapping("/loginpage")
+    public String login(){
+        return "loginpage";
+    }
+
+    @RequestMapping(value = "/loginpage", method= RequestMethod.POST)
+    public String viewHomePage(ModelMap model, @RequestParam String username, @RequestParam String password){
+        if(username.equals("admin")&& password.equals("admin")){
+            return "Dashboard";
+        }
+        else{
+            model.put("error","Incorrect username or password");
+            return "loginpage";
+        }
+    }
 
     @GetMapping("/consumer/details")
     public String showConsumerList(Model model) {
